d748d7a6a005b363b236632a95f9a5bf1f4d3571
Santiago Rios <santiago.rios1758@gmail.com>
2026-08-06 10:12:09 -0500

    ch04: Search Result URLs Chain - subchain search results urls - test subchain search results urls

---
+++ research-summarization-engine/chain_try_3_1.py
@@ -0,0 +1,10 @@
+# Test the sub-chain search results urls
+from utilities import to_obj
+from chain_3_1 import search_results_urls_chain
+
+# test chain invocation
+web_search_str = '{"search_query": "Astorga Spain attractions", "user_question": "What can I see and do in the Spanish town of Astorga?"}'
+web_search_dict = to_obj(web_search_str)
+
+result_urls_list = search_results_urls_chain.invoke(web_search_dict)
+print(result_urls_list)
